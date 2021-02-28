59d0cd4f63e920916fb0bb05e06ca851e5db03bc
faisalsayed10 <faisal.sayed502@gmail.com>
2021-02-28 15:22:44 +0530

    alert dialog when deleting

--- src/components/authentication/Profile.js
+++ src/components/authentication/Profile.js
@@ -1,14 +1,7 @@
 import React, { useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
-import {
-  Alert,
-  AlertIcon,
-  Box,
-  Button,
-  Center,
-  Text,
-} from "@chakra-ui/react";
+import { Alert, AlertIcon, Box, Button, Center, Text } from "@chakra-ui/react";
 import CenterContainer from "../../util/CenterContainer";
 
 export default function Profile() {
@@ -28,54 +21,52 @@ export default function Profile() {
   }
 
   return (
-    <>
-      <CenterContainer>
-        <Box px="6" pt="8" borderWidth="2px" borderRadius="md">
-          <Text as="h2" fontSize="2xl" align="center" mb="4">
-            Profile
-          </Text>
-          {error && (
-            <Alert status="error">
-              <AlertIcon />
-              {error}
-            </Alert>
-          )}
-          <strong>Email:</strong> {currentUser.email}
-          <Link to="/update-profile">
-            <Center>
-              <Button
-                colorScheme="cyan"
-                variant="ghost"
-                w="100"
-                my="4"
-                type="submit"
-              >
-                Update Profile
-              </Button>
-            </Center>
-          </Link>
-        </Box>
-        <Box as="div" w="100" textAlign="center">
+    <CenterContainer>
+      <Box px="6" pt="8" borderWidth="2px" borderRadius="md">
+        <Text as="h2" fontSize="2xl" align="center" mb="4">
+          Profile
+        </Text>
+        {error && (
+          <Alert status="error">
+            <AlertIcon />
+            {error}
+          </Alert>
+        )}
+        <strong>Email:</strong> {currentUser.email}
+        <Link to="/update-profile">
           <Center>
             <Button
               colorScheme="cyan"
               variant="ghost"
-              mt="4"
-              onClick={() => history.push('/')}
+              w="100"
+              my="4"
+              type="submit"
             >
-              Home
-            </Button>
-            <Button
-              colorScheme="cyan"
-              variant="ghost"
-              mt="4"
-              onClick={handleLogout}
-            >
-              Log Out
+              Update Profile
             </Button>
           </Center>
-        </Box>
-      </CenterContainer>
-    </>
+        </Link>
+      </Box>
+      <Box as="div" w="100" textAlign="center">
+        <Center>
+          <Button
+            colorScheme="cyan"
+            variant="ghost"
+            mt="4"
+            onClick={() => history.push("/")}
+          >
+            Home
+          </Button>
+          <Button
+            colorScheme="cyan"
+            variant="ghost"
+            mt="4"
+            onClick={handleLogout}
+          >
+            Log Out
+          </Button>
+        </Center>
+      </Box>
+    </CenterContainer>
   );
 }
--- src/components/files/File.js
+++ src/components/files/File.js
@@ -1,5 +1,16 @@
-import React from "react";
-import { Box, Button, Td, useToast } from "@chakra-ui/react";
+import React, { useRef, useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Box,
+  Button,
+  Td,
+  useToast,
+} from "@chakra-ui/react";
 import { faCopy, faDownload, faMinus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import prettyBytes from "pretty-bytes";
@@ -9,6 +20,8 @@ import { database, storage } from "../../firebase";
 function File({ file }) {
   const { onCopy } = useClipboard(file.url);
   const toast = useToast();
+  const [isOpen, setIsOpen] = useState(false);
+  const cancelRef = useRef();
 
   const handleClick = () => {
     onCopy();
@@ -61,9 +74,39 @@ function File({ file }) {
         </a>
       </Td>
       <Td>
-        <Button onClick={deleteFile} variant="outline" colorScheme="cyan">
+        <Button
+          onClick={() => setIsOpen(true)}
+          variant="outline"
+          colorScheme="cyan"
+        >
           <FontAwesomeIcon icon={faMinus} />
         </Button>
+        <AlertDialog
+          isOpen={isOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={() => setIsOpen(false)}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Delete File
+              </AlertDialogHeader>
+
+              <AlertDialogBody>
+                Are you sure? You can't undo this action afterwards.
+              </AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={() => setIsOpen(false)}>
+                  Cancel
+                </Button>
+                <Button colorScheme="red" onClick={deleteFile} ml={3}>
+                  Delete
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
       </Td>
     </Box>
   );
